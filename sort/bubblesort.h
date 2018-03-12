2b46dfb7ff09af25543ec228e6c7e802a3440ae2
paoqi1997 <30751379+paoqi1997@users.noreply.github.com>
2018-03-13 00:15:18 +0800

    添加了冒泡排序

---
+++ sort/bubblesort.h
@@ -0,0 +1,50 @@
+#ifndef BUBBLESORT_H
+#define BUBBLESORT_H
+
+#include <cstdint>
+
+#include "../modules/scalpel.h"
+
+namespace pits
+{
+    /* Range: m[0] - m[n - 1] */
+    template <typename T>
+    void bubblesort(T m[], uint32_t n)
+    {
+        for (uint32_t i = 0; i < n - 1; ++i)
+        {
+            for (uint32_t j = 0; j < n - i - 1; ++j)
+            {
+                if (m[j] > m[j + 1]) swap(m[j], m[j + 1]);
+            }
+        }
+    }
+
+    /* Range: [pl, pr) */
+    template <typename T>
+    void bubblesort(T *pl, T *pr)
+    {
+        for (int32_t i = 0; i < pr - pl - 1; ++i)
+        {
+            for (int32_t j = 0; j < pr - pl - i - 1; ++j)
+            {
+                if (*(pl + j) > *(pl + j + 1)) swap(*(pl + j), *(pl + j + 1));
+            }
+        }
+    }
+
+    /* Range: [first, last) */
+    template <typename Iterator>
+    void bubblesort(Iterator first, Iterator last)
+    {
+        for (ptrdiff_t i = 0; i < last - first - 1; ++i)
+        {
+            for (ptrdiff_t j = 0; j < last - first - i - 1; ++j)
+            {
+                if (*(first + j) > *(first + j + 1)) swap(*(first + j), *(first + j + 1));
+            }
+        }
+    }
+}
+
+#endif // BUBBLESORT_H
