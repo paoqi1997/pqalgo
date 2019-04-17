c0021a76c1959f74d43f8fe7f520719ff825b206
paoqi1997 <30751379+paoqi1997@users.noreply.github.com>
2019-04-17 17:28:44 +0800

    bubblesort

--- sort/bubblesort.h
+++ sort/bubblesort.h
@@ -6,20 +6,20 @@
 namespace pits
 {
 
-    /**
-     * range: [0, n - 1]
-     */
-    template <typename T>
-    void bubblesort(T m[], int n)
-    {
-        for (int i = 0; i < n - 1; ++i) {
-            for (int j = 0; j < n - i - 1; ++j) {
-                if (m[j] > m[j + 1]) {
-                    std::swap(m[j], m[j + 1]);
-                }
+/**
+ * range: [0, n - 1]
+ */
+template <typename T>
+void bubblesort(T m[], int n)
+{
+    for (int i = 0; i < n - 1; ++i) {
+        for (int j = 0; j < n - i - 1; ++j) {
+            if (m[j] > m[j + 1]) {
+                std::swap(m[j], m[j + 1]);
             }
         }
     }
+}
 
 } // namespace pits
 
