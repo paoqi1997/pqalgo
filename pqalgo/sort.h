73b6836c6f45fd0258857fff4aa5ca0e0c545fe6
paoqi1997 <604869221@qq.com>
2020-05-25 12:08:22 +0800

    pqalgo: selectsort

--- pqalgo/sort.h
+++ pqalgo/sort.h
@@ -8,10 +8,28 @@ namespace pqalgo
 {
 
 template <typename T>
+void selectsort(T m[], std::size_t n)
+{
+    for (std::size_t i = 0; i < n; ++i)
+    {
+        std::size_t idx = i;
+        for (std::size_t j = i + 1; j < n; ++j)
+        {
+            if (m[idx] > m[j]) {
+                idx = j;
+            }
+        }
+        std::swap(m[i], m[idx]);
+    }
+}
+
+template <typename T>
 void bubblesort(T m[], std::size_t n)
 {
-    for (std::size_t i = 0; i < n - 1; ++i) {
-        for (std::size_t j = 0; j < n - i - 1; ++j) {
+    for (std::size_t i = 0; i < n - 1; ++i)
+    {
+        for (std::size_t j = 0; j < n - i - 1; ++j)
+        {
             if (m[j] > m[j + 1]) {
                 std::swap(m[j], m[j + 1]);
             }
--- pqalgo/tests/sort_test.cpp
+++ pqalgo/tests/sort_test.cpp
@@ -1,22 +1,27 @@
 #include <algorithm>
 #include <cassert>
-#include <iterator>
+#include <functional>
 
 #include <pqalgo/sort.h>
+
+using sortFunc = std::function<void(int[], std::size_t)>;
+
+void unit_test(int m[], std::size_t len, const sortFunc& sf)
+{
+    std::size_t n = len;
+    std::for_each(m, m + len, [&n](int& x){ x = int(--n); });
+
+    sf(m, len);
+    assert(std::is_sorted(m, m + len) == true);
+}
 
 int main()
 {
     int m[10];
     std::size_t len = sizeof(m) / sizeof(int);
 
-    std::size_t n = len;
-    std::for_each(std::begin(m), std::end(m), [&n](int& x){ x = int(--n); });
-
-    n = len;
-    pqalgo::bubblesort(m, n);
-
-    bool sorted = std::is_sorted(std::begin(m), std::end(m));
-    assert(sorted == true);
+    unit_test(m, len, pqalgo::selectsort<int>);
+    unit_test(m, len, pqalgo::bubblesort<int>);
 
     return 0;
 }
