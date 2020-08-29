139f903fa0428fbb72fea2dfb80dfe6e00846290
paoqi1997 <604869221@qq.com>
2020-08-29 15:48:14 +0800

    补充注释

--- pqalgo/sort.h
+++ pqalgo/sort.h
@@ -14,6 +14,7 @@ void insertsort(T m[], std::size_t n)
     {
         T tmpVal = m[i];
         std::size_t j = i;
+        // 自右向左遍历元素，比 m[i] 大的元素就往右挪，比 m[i] 小的元素就用 m[i] 替换之
         while (j > 0 && m[j - 1] > tmpVal)
         {
             m[j] = m[j - 1];
@@ -26,6 +27,7 @@ void insertsort(T m[], std::size_t n)
 template <typename T>
 void shellsort(T m[], std::size_t n)
 {
+    // 相比 insertsort 的区别就是 shellsort 是缩小增量排序
     for (std::size_t gap = n / 2; gap > 0; gap /= 2)
     {
         for (std::size_t i = gap; i < n; ++i)
@@ -48,6 +50,7 @@ void selectsort(T m[], std::size_t n)
     for (std::size_t i = 0; i < n - 1; ++i)
     {
         std::size_t minIdx = i;
+        // 自左向右遍历元素，与 m[i] 为起始值，记录最小元素对应的索引，遍历结束后将其与 m[i] 交换
         for (std::size_t j = i + 1; j < n; ++j)
         {
             if (m[minIdx] > m[j]) {
@@ -61,14 +64,20 @@ void selectsort(T m[], std::size_t n)
 template <typename T>
 void __heapsort(T m[], std::size_t rootIndex, std::size_t n)
 {
+    // 面对子树
     T rootVal = m[rootIndex];
+    // 根节点的左叶子节点
     std::size_t leafIndex = rootIndex * 2 + 1;
     while (leafIndex < n)
     {
+        // 右叶子节点存在并且它比左叶子节点要大
         if (leafIndex + 1 < n && m[leafIndex] < m[leafIndex + 1]) {
+            // 取得右叶子节点的索引
             ++leafIndex;
         }
+        // 根节点比叶子节点小
         if (rootVal < m[leafIndex]) {
+            // 把叶子节点换到根节点
             m[rootIndex] = m[leafIndex];
             rootIndex = leafIndex, leafIndex = rootIndex * 2 + 1;
         } else {
@@ -81,11 +90,16 @@ void __heapsort(T m[], std::size_t rootIndex, std::size_t n)
 template <typename T>
 void heapsort(T m[], std::size_t n)
 {
+    // 大根堆有这样的定义：m[i] >= m[2 * i + 1] && m[i] >= m[2 * i + 2]
+    // 小根堆有这样的定义：m[i] <= m[2 * i + 1] && m[i] <= m[2 * i + 2]
+
+    // 构建初始堆，这里是大根堆
     for (int i = n / 2 - 1; i >= 0; --i) {
         __heapsort(m, i, n);
     }
     for (std::size_t i = 1; i < n; ++i)
     {
+        // 交换堆顶元素和末尾元素，使最大元素沉至数组末端
         std::swap(m[0], m[n - i]);
         __heapsort(m, 0, n - i);
     }
