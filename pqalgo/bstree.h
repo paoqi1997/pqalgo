02f8a0aeed44561e1aad7ed9c5f432ab6b6d15cc
paoqi1997 <604869221@qq.com>
2021-01-25 22:49:20 +0800

    bstree添加operator[]等接口

--- pqalgo/bstree.h
+++ pqalgo/bstree.h
@@ -1,6 +1,8 @@
 #ifndef PQALGO_BSTREE_H
 #define PQALGO_BSTREE_H
 
+#include <iostream>
+
 namespace pqalgo
 {
 
@@ -21,8 +23,15 @@ class bstree
 public:
     bstree() : root(nullptr) {}
     ~bstree();
+    V& operator [] (const K& key);
+    void preorder() const;
+    void inorder() const;
+    void postorder() const;
 private:
     void destruct(BSTreeNode<K, V> *node);
+    void __preorder(BSTreeNode<K, V> *node) const;
+    void __inorder(BSTreeNode<K, V> *node) const;
+    void __postorder(BSTreeNode<K, V> *node) const;
 private:
     BSTreeNode<K, V> *root;
 };
@@ -35,6 +44,59 @@ bstree<K, V>::~bstree()
 }
 
 template <typename K, typename V>
+V& bstree<K, V>::operator [] (const K& key)
+{
+    if (root == nullptr) {
+        root = new BSTreeNode<K, V>(key, V());
+        return root->value;
+    }
+
+    auto p = root;
+    auto q = p;
+    while (p != nullptr) {
+        if (p->key < key) {
+            q = p;
+            p = p->left;
+        } else if (p->key > key) {
+            q = p;
+            p = p->right;
+        } else {
+            p->value = V();
+            return p->value;
+        }
+    }
+
+    if (q->key < key) {
+        q->left = new BSTreeNode<K, V>(key, V());
+        return q->left->value;
+    } else {
+        q->right = new BSTreeNode<K, V>(key, V());
+        return q->right->value;
+    }
+}
+
+template <typename K, typename V>
+void bstree<K, V>::preorder() const
+{
+    __preorder(root);
+    std::cout << std::endl;
+}
+
+template <typename K, typename V>
+void bstree<K, V>::inorder() const
+{
+    __inorder(root);
+    std::cout << std::endl;
+}
+
+template <typename K, typename V>
+void bstree<K, V>::postorder() const
+{
+    __postorder(root);
+    std::cout << std::endl;
+}
+
+template <typename K, typename V>
 void bstree<K, V>::destruct(BSTreeNode<K, V> *node)
 {
     if (node == nullptr) {
@@ -43,6 +105,36 @@ void bstree<K, V>::destruct(BSTreeNode<K, V> *node)
     destruct(node->left);
     destruct(node->right);
     delete node;
+}
+
+template <typename K, typename V>
+void bstree<K, V>::__preorder(BSTreeNode<K, V> *node) const
+{
+    if (node != nullptr) {
+        std::cout << node->value << ' ';
+        __preorder(node->left);
+        __preorder(node->right);
+    }
+}
+
+template <typename K, typename V>
+void bstree<K, V>::__inorder(BSTreeNode<K, V> *node) const
+{
+    if (node != nullptr) {
+        __inorder(node->left);
+        std::cout << node->value << ' ';
+        __inorder(node->right);
+    }
+}
+
+template <typename K, typename V>
+void bstree<K, V>::__postorder(BSTreeNode<K, V> *node) const
+{
+    if (node != nullptr) {
+        __postorder(node->left);
+        __postorder(node->right);
+        std::cout << node->value << ' ';
+    }
 }
 
 } // namespace pqalgo
--- pqalgo/tests/tree_test.cpp
+++ pqalgo/tests/tree_test.cpp
@@ -10,6 +10,14 @@ int main()
     cout << "[pqalgo/bstree]" << endl;
     pqalgo::bstree<int, int> bst;
 
+    bst[1] = 1;
+    bst[2] = 2;
+    bst[3] = 3;
+
+    bst.preorder();
+    bst.inorder();
+    bst.postorder();
+
     cout << "ok." << endl;
 
     return 0;
